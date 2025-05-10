bb5cfd8e97fdecfe7e9dec8f54ed44b20edc0fed
Elvis-NChalant <elvispersonaluse@gmail.com>
2025-05-10 12:19:52 +0530

    changed to 0.0.0.0

--- components/ui/login-form.tsx
+++ components/ui/login-form.tsx
@@ -27,7 +27,7 @@ export function LoginForm({
     e.preventDefault();
 
     // Send login request to the Go backend
-    const response = await fetch("http://localhost:8080/login", {
+    const response = await fetch("http://0.0.0.0:8080/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
