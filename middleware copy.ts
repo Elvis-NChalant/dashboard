ff1ccb77e0eff45875042fa49af9d8667c049ac7
Elvis-NChalant <elvispersonaluse@gmail.com>
2025-05-10 13:27:37 +0530

    commented middleware

--- middleware copy.ts
+++ middleware copy.ts
@@ -1,22 +1,22 @@
-// middleware.ts
-import { NextRequest, NextResponse } from "next/server";
+// // middleware.ts
+// import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest) {
+// export function middleware(req: NextRequest) {
 
-  const token = req.cookies.get("auth")?.value;
-  console.log("middleware token");
+//   const token = req.cookies.get("auth")?.value;
+//   console.log("middleware token");
 
-  if (!token) {
-    console.log("middleware token", req.cookies.getAll());
-    console.log("No token found, redirecting to login");
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+//   if (!token) {
+//     console.log("middleware token", req.cookies.getAll());
+//     console.log("No token found, redirecting to login");
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-// Apply to routes that require auth
-export const config = {
-    matcher: "/landing", // protected paths
-};
+// // Apply to routes that require auth
+// export const config = {
+//     matcher: "/landing", // protected paths
+// };
 
